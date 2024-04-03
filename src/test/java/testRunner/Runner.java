cef3fc66e231c8dc34bfafe68a0d62c5c3cfd028
akshaybho <bhogale.akshay11@gmail.com>
2024-04-03 20:49:52 +0530

    added extent

--- src/test/java/testRunner/Runner.java
+++ src/test/java/testRunner/Runner.java
@@ -10,7 +10,8 @@ import org.testng.annotations.DataProvider;
                   monochrome = true,
                  dryRun = false,
                    tags = "@PlaceOrder or @OffersPage",
-        plugin = {"html:target/cucumber.html", "json:target/cucumber.json"}
+        plugin = {"html:target/cucumber.html", "json:target/cucumber.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class Runner extends AbstractTestNGCucumberTests {
 
